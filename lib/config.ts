92e9fd57649c7d8677f0c496124db1ffcdaec596
NguyenDucQuoc-39 <22010234@st.phenikaa-uni.edu.vn>
2025-12-07 13:15:07 +0700

    Deploy to testnet - Package ID: 0xc596d7fef5628ec88f2cd176b3f49f0a7045cae581134f9a5f261d0885cd597c

--- lib/config.ts
+++ lib/config.ts
@@ -9,7 +9,7 @@ import { createNetworkConfig } from "@iota/dapp-kit"
 
 // Package IDs - These will be automatically filled when you run `npm run iota-deploy`
 export const DEVNET_PACKAGE_ID = "0x35ee0c4bbc6fb4cc98768b37ad7d9a3d73641705ab608fdf3f3ed543a0a5e5de"
-export const TESTNET_PACKAGE_ID = ""
+export const TESTNET_PACKAGE_ID = "0xc596d7fef5628ec88f2cd176b3f49f0a7045cae581134f9a5f261d0885cd597c"
 export const MAINNET_PACKAGE_ID = ""
 
 // Network configuration
